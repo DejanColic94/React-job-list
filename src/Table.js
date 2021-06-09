f7966c3bebe9a306fa108967c42a7c6cebf0b641
Dejan Colic <dejan.colic99@gmail.com>
2021-06-09 02:15:53 +0200

    table component rewriten with props

--- src/Table.js
+++ src/Table.js
@@ -1,32 +1,41 @@
 import React, {Component} from 'react'
 
-class Table extends Component {
+
+const TableHeader = () => {
+    return (
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Job</th>
+        </tr>
+      </thead>
+    )
+  }
+
+  const TableBody = (props) => {
+    const rows = props.characterData.map((row, index) => {
+      return (
+        <tr key={index}>
+          <td>{row.name}</td>
+          <td>{row.job}</td>
+        </tr>
+      )
+    })
+  
+    return <tbody>{rows}</tbody>
+  }
+
+  class Table extends Component {
     render() {
-        return (
-            <table>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Job</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Mac</td>
-                        <td>Bouncer</td>
-                    </tr>
-                    <tr>
-                        <td>Dee</td>
-                        <td>Aspiring actress</td>
-                    </tr>
-                    <tr>
-                        <td>Denis</td>
-                        <td>Bartender</td>
-                    </tr>
-                </tbody>
-            </table>
-        )
+        const {characterData} = this.props
+      return (
+        <table>
+          <TableHeader />
+          <TableBody characterData={characterData} />
+        </table>
+      )
     }
-}
+  }
+
 
 export default Table
