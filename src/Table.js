64e089216bcabfe37bdd5ec01ad8566743a347a0
Dejan Colic <dejan.colic99@gmail.com>
2021-06-09 02:29:55 +0200

    Added delete button, (states)

--- src/Table.js
+++ src/Table.js
@@ -18,6 +18,9 @@ const TableHeader = () => {
         <tr key={index}>
           <td>{row.name}</td>
           <td>{row.job}</td>
+          <td>
+            <button onClick={() => props.removeCharacter(index)}>Delete</button>
+          </td>
         </tr>
       )
     })
@@ -25,16 +28,15 @@ const TableHeader = () => {
     return <tbody>{rows}</tbody>
   }
 
-  class Table extends Component {
-    render() {
-        const {characterData} = this.props
-      return (
-        <table>
-          <TableHeader />
-          <TableBody characterData={characterData} />
-        </table>
-      )
-    }
+  const Table = (props) => {
+    const {characterData, removeCharacter} = props
+  
+    return (
+      <table>
+        <TableHeader />
+        <TableBody characterData={characterData} removeCharacter={removeCharacter} />
+      </table>
+    )
   }
 
 
